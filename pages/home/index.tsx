4246aa5604907a73ade69ff2e6c52fe19ecacf05
RaayNoff <kraxtv471365@gmail.com>
2022-11-17 17:43:27 +0700

    Finished home page for now

---
+++ pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import MainLayout from "../../layouts/MainLayout.component";
+
+const Home: FC = () => {
+	return <MainLayout></MainLayout>;
+};
+
+export default Home;
