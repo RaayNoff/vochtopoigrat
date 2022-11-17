aae2c9f5f2cd26f2ba72c7376e972acd0f84b833
RaayNoff <kraxtv471365@gmail.com>
2022-11-17 17:43:11 +0700

    Added redirect to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <>Залупа</>;
+import { useRouter } from "next/router";
+
+import { useEffect } from "react";
+
+import { Routes } from "../models/enums/Routes";
+
+export default function () {
+	const router = useRouter();
+	useEffect(() => {
+		router.push(Routes.HOME);
+	}, []);
+
+	return <></>;
 }
