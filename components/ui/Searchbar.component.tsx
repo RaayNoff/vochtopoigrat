23dc550d032664b73eac486bcb5c2f589f8c3c29
RaayNoff <kraxtv471365@gmail.com>
2022-11-17 17:46:21 +0700

    Added searchbar ui component

---
+++ components/ui/Searchbar.component.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+import { FC } from "react";
+import { SlMagnifier } from "react-icons/sl";
+
+import s from "../../styles/components/ui/Searchbar.module.scss";
+
+import Button from "./Button.component";
+
+const Searchbar: FC = () => {
+	const searchCallback = () => {
+		console.log("callback");
+	};
+
+	return (
+		<section className={clsx(s.searchbar)}>
+			<div className={clsx(s.searchbar__loupe)}>
+				<SlMagnifier />
+			</div>
+			<input
+				id="search"
+				type="text"
+				className={clsx(s.searchbar__input)}
+				placeholder="Search games"
+			/>
+			<Button
+				className={clsx(s.searchbar__button)}
+				callback={() => searchCallback}
+			>
+				Search
+			</Button>
+		</section>
+	);
+};
+
+export default Searchbar;
