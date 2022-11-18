61bf0c0d0c59c11bdd21537efc9939ee7ec5b65b
RaayNoff <kraxtv471365@gmail.com>
2022-11-18 19:54:35 +0700

    Added placeholders for all pages

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 import MainLayout from "../../layouts/MainLayout.component";
 
 const Home: FC = () => {
-	return <MainLayout testid="home-page"></MainLayout>;
+	return <MainLayout testid="home-page">HOME!</MainLayout>;
 };
 
 export default Home;
---
+++ pages/random/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+import MainLayout from "../../layouts/MainLayout.component";
+
+const Random: FC = () => {
+	return <MainLayout testid="random-page">RANDOM!</MainLayout>;
+};
+
+export default Random;
