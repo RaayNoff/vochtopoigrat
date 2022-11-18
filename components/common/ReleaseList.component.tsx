0c0f06ca36e2049fb86a3cc7bffdd4dd0f91ea88
evgeniy novakovskiy <allidon93@gmail.com>
2022-11-18 22:26:57 +0700

    Started work on release list component

---
+++ components/common/ReleaseList.component.tsx
@@ -0,0 +1,133 @@
+import axios from "axios";
+import clsx from "clsx";
+import { useState, useEffect } from "react";
+
+import styles from "../../styles/components/common/ReleaseList.module.scss";
+
+interface gameListItem {
+  added: number;
+  added_by_status: {
+    yet: number;
+    owned: number;
+    beaten: number;
+    toplay: number;
+    dropped: number;
+    playing: number;
+  };
+  background_image: string;
+  clip: null;
+  dominant_color: string;
+  esrb_rating: { id: number; name: string; slug: string };
+  genres: {
+    games_count: number;
+    id: number;
+    image_background: string;
+    name: string;
+    slug: string;
+  }[];
+  id: number;
+  metacritic: number;
+  name: string;
+  parent_platforms: { id: number; name: string; slug: string }[];
+  platforms: {
+    platform: {
+      games_count: number;
+      id: number;
+      image: null;
+      image_background: string;
+      name: string;
+      slug: string;
+      year_end: null;
+      year_start: null;
+    };
+    released_at: string;
+    requirements_en: { minimum: string; recommended: string };
+    requirements_ru: null;
+  }[];
+  playtime: number;
+  rating: number;
+  rating_top: number;
+  ratings: { count: number; id: number; percent: number; title: string }[];
+  ratings_count: number;
+  released: string;
+  reviews_count: number;
+  reviews_text_count: number;
+  saturated_color: string;
+  short_screenshots: { id: number; image: string }[];
+  slug: string;
+  stores: {
+    id: number;
+    store: {
+      domain: string;
+      games_count: number;
+      id: number;
+      image_background: string;
+      name: string;
+      slug: string;
+    };
+  }[];
+  suggestions_count: number;
+  tags: {
+    games_count: number;
+    id: number;
+    image_background: string;
+    language: string;
+    name: string;
+    slug: string;
+  }[];
+  tba: boolean;
+  updated: string;
+  user_game: null;
+}
+
+const ReleaseList: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [data, setData] = useState<gameListItem[]>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get("https://api.rawg.io/api/games?key=5942455621404e6dae027f9513f71abb")
+      .then((res) => res.data.results)
+      .then((res) => {
+        setData(res);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return !isLoading ? (
+    <section className={clsx(styles.latest_games)}>
+      <ul className={styles.latest_games__list}>
+        {data?.map((game) => (
+          <li className={styles.latest_games__item} key={game.id}>
+            <a href="#" className={styles.latest_games__link}>
+              {game.name}
+              <ul className={styles.latest_games__tags_list}>
+                {game.tags.map((tag) => (
+                  <li className={styles.latest_games__tags_item} key={tag.id}>
+                    {tag.name}
+                  </li>
+                ))}
+              </ul>
+              <ul className={styles.latest_games__platform_list}>
+                {game?.parent_platforms.map((platform) => (
+                  <li
+                    className={styles.latest_games__platform_item}
+                    key={platform.id}
+                  >
+                    {platform.name}
+                  </li>
+                ))}
+              </ul>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default ReleaseList;
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react";
 
+import ReleaseList from "../../components/common/ReleaseList.component";
+
 import MainLayout from "../../layouts/MainLayout.component";
 
 const Home: FC = () => {
-	return <MainLayout></MainLayout>;
+  return (
+    <MainLayout>
+      <ReleaseList />
+    </MainLayout>
+  );
 };
 
 export default Home;
