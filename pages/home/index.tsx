5bfb3e397f082013d6674fd3e6f39d518d143599
RaayNoff <kraxtv471365@gmail.com>
2022-11-20 20:38:31 +0700

    Added getServerSideProps for Home page

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,19 +1,52 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, useMemo } from "react";
 
 import Carousel from "../../components/common/Carousel.component";
 
 import ReleaseList from "../../components/common/ReleaseList.component";
+import { useActions } from "../../hooks/useActions";
 
 import MainLayout from "../../layouts/MainLayout.component";
+import { IGameList, Result } from "../../models/interfaces/IGameList";
 
-const Home: FC = () => {
+interface IHomeProps {
+	games: Result[];
+}
+
+const Home: FC<IHomeProps> = ({ games }) => {
+	const { setGames } = useActions();
+
+	useMemo(() => {
+		setGames(games);
+	}, [games]);
+
 	return (
 		<MainLayout testid="home-page">
 			<Carousel></Carousel>
 
-			{/* <ReleaseList /> */}
+			<ReleaseList />
 		</MainLayout>
 	);
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	let games;
+
+	try {
+		const response = await axios.get<IGameList>(
+			`${process.env.API_URL}?key=${process.env.API_KEY}`,
+		);
+
+		games = response.data.results;
+	} catch (error) {
+		console.log(error);
+	}
+
+	return {
+		props: {
+			games,
+		},
+	};
+};
