c68f908d5e0acc74413a46481d2f4b2b26ed4309
RaayNoff <kraxtv471365@gmail.com>
2022-11-23 18:04:27 +0700

    Added Intersection Observer Api Mock for test compatability

--- __tests__/ActiveLink.test.jsx
+++ __tests__/ActiveLink.test.jsx
@@ -8,25 +8,29 @@ import Charts from "../pages/charts";
 import Releases from "../pages/releases";
 
 import { renderWithRedux } from "../helpers/renderWithRedux.js";
+import { setupIntersectionObserverMock } from "../helpers/setupIntersectionObserverMock.js";
 
 //Mocking next router + links
 jest.mock("next/router", () => require("next-router-mock"));
 jest.mock("next/dist/client/router", () => require("next-router-mock"));
 
 describe("ActiveLink test", () => {
-	test("Home link", () => {
-		mockRouter.push("/home");
-		renderWithRedux(<Home />);
-		const homeLink = screen.getByTestId("home-link");
-		const randomLink = screen.getByTestId("random-link");
-		const chartsLink = screen.getByTestId("charts-link");
-		const releasesLink = screen.getByTestId("releases-link");
+	beforeEach(() => {
+		setupIntersectionObserverMock();
+	}),
+		test("Home link", () => {
+			mockRouter.push("/home");
+			renderWithRedux(<Home />);
+			const homeLink = screen.getByTestId("home-link");
+			const randomLink = screen.getByTestId("random-link");
+			const chartsLink = screen.getByTestId("charts-link");
+			const releasesLink = screen.getByTestId("releases-link");
 
-		expect(homeLink.classList.contains("active")).toBe(true);
-		expect(randomLink.classList.contains("active")).toBe(false);
-		expect(chartsLink.classList.contains("active")).toBe(false);
-		expect(releasesLink.classList.contains("active")).toBe(false);
-	});
+			expect(homeLink.classList.contains("active")).toBe(true);
+			expect(randomLink.classList.contains("active")).toBe(false);
+			expect(chartsLink.classList.contains("active")).toBe(false);
+			expect(releasesLink.classList.contains("active")).toBe(false);
+		});
 
 	test("Random link", () => {
 		mockRouter.push("/random");
