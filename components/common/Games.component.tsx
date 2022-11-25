05b1cb6b19acf7854b8198794fe0a76aa9f7e01d
RaayNoff <kraxtv471365@gmail.com>
2022-11-26 01:11:56 +0700

    Added filters to Games component

--- components/common/Games.component.tsx
+++ components/common/Games.component.tsx
@@ -1,14 +1,21 @@
+import clsx from "clsx";
 import { FC } from "react";
 
 import s from "../../styles/components/common/Games.module.scss";
 
+import Filters from "./Filters.component";
 import ReleaseList from "./ReleaseList.component";
 
-const Games: FC = () => {
+interface IGamesProps {
+	className?: string;
+}
+
+const Games: FC<IGamesProps> = ({ className }) => {
 	return (
-		<section className={s.games}>
+		<section className={clsx(s.games, className)}>
 			<div className="container">
 				<div className={s.games__container}>
+					<Filters />
 					<ReleaseList />
 				</div>
 			</div>
