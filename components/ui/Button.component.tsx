05ef068d3b1bc4c016c251d8820b7112e2eac0fe
RaayNoff <kraxtv471365@gmail.com>
2022-11-26 01:11:06 +0700

    Prop callback changed

--- components/ui/Button.component.tsx
+++ components/ui/Button.component.tsx
@@ -6,7 +6,7 @@ import s from "../../styles/components/ui/Button.module.scss";
 interface IButtonProps {
 	children?: React.ReactNode;
 	className?: string;
-	callback?: () => void;
+	callback?: (...args: any) => void;
 }
 
 const Button: FC<IButtonProps> = ({ children, callback, className }) => {
