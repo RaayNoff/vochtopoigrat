3fca933283786f5f74abd4b7a427361388d5a861
RaayNoff <kraxtv471365@gmail.com>
2022-12-08 00:51:08 +0700

    N: Added loader to SearchGameItem

--- components/ui/SearchGameItem.component.tsx
+++ components/ui/SearchGameItem.component.tsx
@@ -1,11 +1,13 @@
 import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { useSearchedGameById } from "../../hooks/useSearchedGameById";
 import { Routes } from "../../models/enums/Routes";
 import s from "../../styles/components/ui/SearchGameItem.module.scss";
+
+import ImageLoader from "./ImageLoader.component";
 
 interface ISearchGameItemProps {
 	gameId: number;
@@ -13,6 +15,11 @@ interface ISearchGameItemProps {
 
 const SearchGameItem: FC<ISearchGameItemProps> = ({ gameId }) => {
 	const game = useSearchedGameById(gameId);
+	const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+	const onGameLoaded = () => {
+		setIsImageLoaded(true);
+	};
 
 	if (!game) return null;
 
@@ -21,14 +28,21 @@ const SearchGameItem: FC<ISearchGameItemProps> = ({ gameId }) => {
 			<div className={clsx(s.gameLink__container)}>
 				<div className={clsx(s.gameLink__imageContainer, s.imageContainer)}>
 					<Image
-						className={clsx(s.imageContainer__img)}
+						className={clsx(
+							s.imageContainer__img,
+							!isImageLoaded && s.imageContainer__hidden,
+						)}
 						src={game.background_image || "/assets/images/noImage.jpg"}
 						alt={game.name}
 						width={500}
 						height={500}
 						loading="lazy"
 						placeholder="empty"
+						onLoadingComplete={onGameLoaded}
 					/>
+					{!isImageLoaded && game.background_image && (
+						<ImageLoader className={s.imageLoader} />
+					)}
 				</div>
 				<p className={clsx(s.gameLink__name)}>{game.name}</p>
 			</div>
