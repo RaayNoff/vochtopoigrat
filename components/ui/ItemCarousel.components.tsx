1b2be7aff3a180739a56f1417ccb554f9895d061
RaayNoff <kraxtv471365@gmail.com>
2022-12-09 23:20:44 +0700

    Changed styles for link

--- components/ui/ItemCarousel.components.tsx
+++ components/ui/ItemCarousel.components.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { FC, memo, MouseEvent } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
@@ -9,32 +9,45 @@ type ItemCarouselType = {
 	url: string;
 	index: number;
 	slideAction: number;
-	onClickSlider: (event: React.MouseEvent) => void;
+	onClickSlider: (event: MouseEvent) => void;
 	length: number;
 };
 
 import s from "../../styles/components/common/Carousel.module.scss";
 
-export const ItemCarousel = React.memo(function ItemCarousel({
-	id,
-	title,
-	url,
-	slideAction,
-	onClickSlider,
-	index,
-	length,
-}: ItemCarouselType) {
-	const [isHover, setIsHover] = useState(false);
+export const ItemCarousel: FC<ItemCarouselType> = memo(
+	({ id, title, url, slideAction, onClickSlider, index, length }) => {
+		if (index == slideAction) {
+			return (
+				<div className={clsx(s.slide, s.slide__active, s.slide_hover)}>
+					<Image
+						onClick={(event) => onClickSlider(event)}
+						width={1152}
+						height={648}
+						src={url || "/assets/image/noImage.jpg"}
+						alt={title}
+						loading="lazy"
+						placeholder="empty"
+					/>
+					<Link href={`/games/${id}`}>{title}</Link>
+				</div>
+			);
+		}
 
-	if (isHover && index == slideAction) {
 		return (
 			<div
-				className={clsx(s.slide, s.slide__active, s.slide_hover)}
-				onMouseOut={() => setIsHover(false)}
-				onMouseOver={() => setIsHover(true)}
+				className={clsx(
+					s.slide,
+					s.slide__hidden,
+					index == slideAction && s.slide__active,
+					index - 1 == slideAction && s.slide__next,
+					slideAction + 1 == length && index == 0 && s.slide__next,
+					index + 1 == slideAction && s.slide__prev,
+					slideAction == 0 && index + 1 == length && s.slide__prev,
+				)}
+				onClick={(event) => onClickSlider(event)}
 			>
 				<Image
-					onClick={(event) => onClickSlider(event)}
 					width={1152}
 					height={648}
 					src={url || "/assets/image/noImage.jpg"}
@@ -42,39 +55,7 @@ export const ItemCarousel = React.memo(function ItemCarousel({
 					loading="lazy"
 					placeholder="empty"
 				/>
-				<Link
-					href={`/games/${id}`}
-					onMouseOver={() => setIsHover(true)}
-					onMouseOut={() => setIsHover(false)}
-				>
-					OPEN PAGE
-				</Link>
 			</div>
 		);
-	}
-
-	return (
-		<div
-			className={clsx(
-				s.slide,
-				s.slide__hidden,
-				index == slideAction && s.slide__active,
-				index - 1 == slideAction && s.slide__next,
-				slideAction + 1 == length && index == 0 && s.slide__next,
-				index + 1 == slideAction && s.slide__prev,
-				slideAction == 0 && index + 1 == length && s.slide__prev,
-			)}
-			onClick={(event) => onClickSlider(event)}
-			onMouseOver={() => setIsHover(true)}
-		>
-			<Image
-				width={1152}
-				height={648}
-				src={url || "/assets/image/noImage.jpg"}
-				alt={title}
-				loading="lazy"
-				placeholder="empty"
-			/>
-		</div>
-	);
-});
+	},
+);
